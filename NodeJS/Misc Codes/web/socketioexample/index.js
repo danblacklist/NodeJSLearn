b3f0baf0e183280becd205e6a5123c13bab5875a
Tamboli <672243@cognizant.com>
2018-07-30 16:26:05 +0530

    Socket.io example added

---
+++ NodeJS/Misc Codes/web/socketioexample/index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.broadcast.emit('Hi');
+  console.log(socket.client.id+' connected');
+  socket.on('disconnect', function() {
+    console.log(socket.client.id+' disconnected');
+});
+socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
